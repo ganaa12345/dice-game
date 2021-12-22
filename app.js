da81e3514448163ce88d1f74926d9946e4f33c62
Ganaa <ganaagombo8@gmail.com>
2021-12-22 21:26:24 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ var scores = [0, 0]
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шоо аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-//<div class="player-score" id="score-0">43</div>
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
-//window.document.querySelector('#score-1').innerHTML = "<em>YES!</em>"
-window.document.querySelector('.dice').style.display
+
+document.getElementById("score-0").textContent = '0';
+// window.document.querySelector('#score-0').textContent = 0;
+window.document.getElementById('score-1').textContent = '0';
+window.document.getElementById('current-0').textContent = '0';
+window.document.getElementById('current-1').textContent = '0';
+var diceDocument = window.document.querySelector('.dice');
+diceDocument.style.display = "none";
+document.querySelector('.btn-roll').addEventListener("click", function () {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDocument.style.display = "block";
+    diceDocument.src = 'dice-' + diceNumber + '.png';
+
+});
+
 console.log('Шоо : ' + dice);
